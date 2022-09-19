4451c88ecbc5212775c04b5a64d54208fff452d0
Larry741 <85595046+Larry741@users.noreply.github.com>
2022-09-19 15:19:33 +0100

    Fix Navbar import statement

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
 import Expenses from "../components/Expenses/Expenses";
 import NewExpense from "../components/NewExpenses/NewExpense";
-import { useState } from "react";
 
 import styles from "../styles/home.module.scss";
-import Navbar from "../components/navbar/navbar";
+import Navbar from "../components/Navbar/Navbar";
 
 function Index() {
   return (
